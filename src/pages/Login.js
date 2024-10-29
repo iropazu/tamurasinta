e95cd75aa8da5deb7483a3e3f45f99d3315862de
fujimuraK4zuki <k.mattibo@gmail.com>
2024-10-29 19:26:45 +0900

    Loginの作成

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Login = () => {
-  return <div>Login</div>
+  return <div>Login
+    <div>メールアドレス</div>
+    <input type = "text"></input>
+    <div>パスワード</div>
+    <input type = "text"></input>
+  </div>
 }
 
 export default Login
