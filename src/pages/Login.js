049adf09217dd8d9b46653a4bd805952eb8c899b
fujimuraK4zuki <k.mattibo@gmail.com>
2024-10-29 19:55:59 +0900

    文字の大きさ変更

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Login = () => {
-  return <div>ログイン
+  return <div><h2>ログイン</h2>
     <div>メールアドレス</div>
     <input type = "text"></input>
     <div>パスワード</div>
     <input type = "text"></input>
     <div>または</div>
     <input type = "text"></input>
+    
   </div>
 }
 
