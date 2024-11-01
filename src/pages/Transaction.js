f5957cc47d6054bd72bdcb234670e7ec6db50348
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-11-01 10:15:14 +0900

    Adjust margin-top in Footer component and update styles in Transaction page

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -5,46 +5,56 @@ import SendIcon from '@mui/icons-material/Send'
 
 const Transaction = () => {
   return (
-    <div className={styles.transaction_container}>
-      <div className={styles.trading_information}>
+    <div className={styles.transactionContainer}>
+      <div className={styles.transactionDetails}>
         <h5>取引情報</h5>
-        <div className={styles.item_title}>
+        <div className={styles.itemInfo}>
           <img src={sample1} alt="sample1"></img>
           <p>タイトル</p>
         </div>
-        <div className={styles.item_detail}>
-          <div className={styles.inf}>
+        <div className={styles.itemDetails}>
+          <div className={styles.infoRow}>
             商品代金<p>￥1000</p>
           </div>
-          <div className={styles.inf}>
+          <div className={styles.infoRow}>
             購入日時<p>2024年10月23日 17:51</p>
           </div>
-          <div className={styles.inf}>
+          <div className={styles.infoRow}>
             商品ID<p>m73319947785</p>
           </div>
         </div>
       </div>
-      <div className={styles.trading_screen}>
+      <div className={styles.transactionScreen}>
         <h2>取引画面</h2>
         <h5>出品者情報</h5>
-        <div className={styles.user_information}>
+        <div className={styles.sellerInfo}>
           <img src={sample1} alt="sample1"></img>
           <p>name</p>
         </div>
         <div>
           <h5>メッセージ</h5>
-          <div className={styles.user_message}>
+          <div className={styles.messageContainer}>
             <div className={styles.message}>
+              <img src={sample1} alt="sample1"></img>
+              <span>hogehogehoge</span>
+              <img src={sample1} alt="sample1"></img>
+              <span>hogehogehoge</span>
+              <img src={sample1} alt="sample1"></img>
+              <span>hogehogehoge</span>
+              <img src={sample1} alt="sample1"></img>
+              <span>hogehogehoge</span>
+              <img src={sample1} alt="sample1"></img>
+              <span>hogehogehoge</span>
               <img src={sample1} alt="sample1"></img>
               <span>hogehogehoge</span>
             </div>
           </div>
-        </div>
-        <div className={styles.send_container}>
-          <input className={styles.send} />
-          <button>
-            <SendIcon className={styles.bt} />
-          </button>
+          <div className={styles.messageInputContainer}>
+            <input className={styles.messageInput} />
+            <button>
+              <SendIcon />
+            </button>
+          </div>
         </div>
       </div>
     </div>
