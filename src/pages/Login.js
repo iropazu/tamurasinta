0c325440a6be62026877fe1e36b253aca97737cb
fujimuraK4zuki <k.mattibo@gmail.com>
2024-11-01 19:27:47 +0900

    ボタンの追加

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,15 +4,15 @@ import style from '../styles/Login.module.css'
 const Login = () => {
   return <div><body className={style.body}>ログイン</body>
   
-    <div><body className={style.body}>メールアドレス</body></div>
+    <div><body className={style.p}>メールアドレス</body></div>
     <input type = "text"></input>
     
-    <div><body className={style.body}>パスワード</body></div>
+    <div><body className={style.p}>パスワード</body></div>
     
     <input type = "text"></input>
     <div>または</div>
-    <input type = "text"placeholder="Googleで登録"  ></input>
-    <img src="https://developers.google.com/identity/images/g-logo.png" alt={style.body}></img>
+    
+    <button className={style.button}><img src="https://developers.google.com/identity/images/g-logo.png" alt={style.picture}/></button>
     
     <button body className={style.body}>ログイン</button>
   </div>
