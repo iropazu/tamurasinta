b39ff54c1cd2ea5accba49c628855bc3efe54ed3
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2024-11-05 14:18:39 +0900

    変数名調整、入力内容の確認

--- src/services/bookUpload.js
+++ src/services/bookUpload.js
@@ -1,40 +1,36 @@
-import { addDoc, collection, } from "firebase/firestore"
-import { auth, db, storage } from "../firebase/firebase"
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
+import { addDoc, collection } from 'firebase/firestore'
+import { auth, db, storage } from '../firebase/firebase'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 
+export const addImage = async (imageFileList) => {
+  try {
+    const imageArray = Array.from(imageFileList)
+    const imagePromises = imageArray.map(async (image, _) => {
+      const storageRef = ref(storage, `bookImage/${Date.now()}_${image.name}`)
 
-export const addImage=async(imagefileList)=>{
-  try{
-    const imageArray=Array.from(imagefileList)
-    const imagePromies=imageArray.map(async(image,_)=>{
-      const storageRef=ref(storage,`bookImage/${Date.now()}_${image.name}`)
-
-      await uploadBytes(storageRef,image)
-      const imageUrl=await getDownloadURL(storageRef)
+      await uploadBytes(storageRef, image)
+      const imageUrl = await getDownloadURL(storageRef)
       return imageUrl
     })
 
-    const images=await Promise.all(imagePromies)
+    const images = await Promise.all(imagePromises)
     return images
-  }catch(error){
-    console.log('addImageのエラー:',error)
+  } catch (error) {
+    console.log('addImageのエラー:', error)
   }
-
 }
 
-export const bookUpload=async(imagefileList,bookInfo)=>{
-  const Url=await addImage(imagefileList)
-  const user=auth.currentUser
-  try{
-    await addDoc(collection(db,'books'),{
+export const bookUpload = async (imagefileList, bookInfo) => {
+  const Url = await addImage(imagefileList)
+  const user = auth.currentUser
+  try {
+    await addDoc(collection(db, 'books'), {
       ...bookInfo,
-      bookImageUrl:Url,
-      userId:user.uid,
-      state:false,
-      timeData:new Date()
+      bookImageUrl: Url,
+      userId: user.uid,
+      timeData: new Date(),
     })
-  }catch(error){
-    console.log('bookUploadのエラー:',error)
+  } catch (error) {
+    console.log('bookUploadのエラー:', error)
   }
-  
 }
