bd539c09b65f7dd14d9dc5f84b1b635fee6e28cc
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2024-11-06 13:43:48 +0900

    プロフィール画像をgoogleアカウントの画像に

--- src/components/Header/MainHeader.js
+++ src/components/Header/MainHeader.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './MainHeader.css'
 import Button from '../Button/Button'
 import FavoriteIcon from '@mui/icons-material/Favorite'
@@ -6,8 +6,24 @@ import MessageIcon from '@mui/icons-material/Message'
 import NotificationsIcon from '@mui/icons-material/Notifications'
 import noImg from '../../assets/image/noImg.jpg'
 import { Link } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../../firebase/firebase'
 
 const MainHeader = () => {
+
+  const [imageProfile,setImageProfile]=useState(null)
+
+  useEffect(()=>{
+    const unsubscribe=onAuthStateChanged(auth,(user)=>{
+      if (user){
+        setImageProfile(user?.photoURL)
+      } else {
+        setImageProfile(noImg)
+      }
+    })
+    return ()=>unsubscribe();
+  },[])
+
   return (
     <header className="main-header-container">
       <div className="right-contents">
@@ -16,7 +32,7 @@ const MainHeader = () => {
           <MessageIcon className="action-icon" style={{ fontSize: 32 }} />
           <NotificationsIcon className="action-icon" style={{ fontSize: 32 }} />
         </div>
-        <img src={noImg} alt="profile" className="profile-image" />
+        <img src={imageProfile} alt="profile" className="profile-image" />
         <Link to={'/create-listing'}>
           <Button className="listing-button">出品</Button>
         </Link>
