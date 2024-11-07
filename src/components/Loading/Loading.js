2e94d88d31dac8da84ad43905b1428008c9f17c2
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-11-07 18:10:32 +0900

    classNameに変更

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,14 +3,14 @@ import styles from "./Loading.module.css"
 
 const Loading = () => {
   return (
-    <div class={styles.mikepad_loading}>
-      <div class={styles.binding}></div>
-      <div class={styles.pad}>
-        <div class={styles.line_line1}></div>
-        <div class={styles.line_line2}></div>
-        <div class={styles.line_line3}></div>
+    <div className={styles.mikepad_loading}>
+      <div className={styles.binding}></div>
+      <div className={styles.pad}>
+        <div className={styles.line_line1}></div>
+        <div className={styles.line_line2}></div>
+        <div className={styles.line_line3}></div>
       </div>
-      <div class={styles.text}>mikepad is loading...</div>
+      <div className={styles.text}>mikepad is loading...</div>
     </div>
   )
 }
